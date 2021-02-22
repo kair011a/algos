00de5513c3fc339296c6f0a52d57090b66f1deb0
Kaiyrkhan Kairolla <58839934+kair011a@users.noreply.github.com>
2021-02-22 15:09:33 +0600

    Поиск мостов O(n+m)

---
+++ Bridges.cpp
@@ -0,0 +1,32 @@
+const int mxN=1e5+7;
+vector<int> g[mxN];
+bool used[mxN];
+timer, tin[mxN], fup[mxN];
+ 
+void dfs (int v, int p = -1) {
+	used[v] = 1;
+	tin[v] = fup[v] = timer++;
+	for(int to:g[v]) {
+    if (to == p)  continue;
+		if (used[to]) fup[v] = min (fup[v], tin[to]);
+		else {
+			dfs (to, v);
+			fup[v] = min (fup[v], fup[to]);
+			if (fup[to] > tin[v]) {
+				IS_BRIDGE(v,to);
+      }
+		}
+	}
+}
+ 
+void find_bridges() {
+	timer = 0;
+	for (int i=0; i<n; i++) {
+		used[i] = 0;
+  }
+	for (int i=0; i<n; i++){
+		if (!used[i]) {
+			dfs (i);
+    }
+  }
+}
