4c384fb77d74cbd01c11d940752a279a321ff8e5
Kaiyrkhan Kairolla <58839934+kair011a@users.noreply.github.com>
2021-02-23 15:04:52 +0600

    Update z-function.cpp

--- z-function.cpp
+++ z-function.cpp
@@ -4,12 +4,12 @@
 vector<int> z_function_trivial (string s) {
 	int n = (int)s.size();
 	vector<int> z (n);
-  z[0] = 0;
+ 	z[0] = 0;
 	for (int i=1; i<n; ++i){
 		while (i + z[i] < n && s[z[i]] == s[i+z[i]]){
-		  z[i]++;  
-    }
-  }
+			z[i]++;  
+    	}
+  	}
 	return z;
 }
 
